863ffd094d63133980f6f052ea518a9c28e50662
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-08-20 15:15:39 +0000

    Add post_detail template and link its path in views.py and urls.py.

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),]
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),]
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.views import generic
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .models import Post, Comment
-# from .forms import CommentForm
+
 
 # Create your views here.
 
@@ -26,3 +26,23 @@ class PostList(generic.ListView):
     template_name = "blog/index.html"
     paginate_by = 6
 
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    return render(
+        request, 
+        "blog/post_detail.html", 
+        {"post": post},
+        )
