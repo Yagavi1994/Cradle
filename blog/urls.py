12843f12f700bd6123e94ca74b96993c7a7446ca
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-08-21 19:43:04 +0000

    Add trans tag in logout.html.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
-from blog.forms import CustomPasswordResetForm
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(form_class=CustomPasswordResetForm), name='password_reset'),]
+]
--- cradle/settings.py
+++ cradle/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-_(^ge47_(a$1bl!)xg+l2c)$uxza2vy=tu7x6yev8t+@pmnm#!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-yagavi1994-cradle-5ckw8ika6zj.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django_summernote',
     'blog',
     'about',
+    'anymail',
 ]
 
 SITE_ID = 1
@@ -154,12 +155,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email settings for SMTP backend
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_PORT = 587  # SMTP port for TLS
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("Username")
-EMAIL_HOST_PASSWORD = os.environ.get("Password")
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 # Default "from" email address
 DEFAULT_FROM_EMAIL = 'cradle.blog.2024@gmail.com'
