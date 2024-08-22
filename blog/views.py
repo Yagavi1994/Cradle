8eb771a98516474d5bb72c17ebd6112eaf25030f
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-08-22 13:14:26 +0000

    Add comments and comment form.

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .models import Post, Comment
+from .forms import CommentForm
 
 
 # Create your views here.
@@ -41,8 +42,23 @@ def post_detail(request, slug):
     """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+
+    comment_form = CommentForm()
+
     return render(
         request, 
         "blog/post_detail.html", 
-        {"post": post},
+        {"post": post,
+        "comments": comments,
+        "comment_count": comment_count,
+        "comment_form" : CommentForm,},
         )
