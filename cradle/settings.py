ae9d546c6b771dfc337bc3690ba2e61002ae82d6
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-08-29 15:36:36 +0000

    Change settings for backend email to gmail.

--- cradle/settings.py
+++ cradle/settings.py
@@ -194,8 +194,8 @@ PASSWORD_HASHERS = [
 
 # Email settings for SMTP backend
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
